42c133e5c6f1ecc26128793276b553d77b2f7505
MarcoMontanelli <101709469+MarcoMontanelli@users.noreply.github.com>
2023-09-04 10:40:42 +0000

    areoportop

---
+++ areoporto/src/main/java/areoporto/Cliente.java
@@ -0,0 +1,63 @@
+package areoporto;
+
+class Cliente {
+    static public int codici = 0;
+    private int codiceCliente;
+    private String cognome;
+    private String nome;
+    private String nazioneNascita;
+    private String cittaNascita;
+    private String dataNascita;
+
+    public Cliente(String cognome, String nome, String nazioneNascita, String cittaNascita, String dataNascita) {
+        codiceCliente = codici;
+        codici++;
+        this.cognome = cognome;
+        this.nome = nome;
+        this.nazioneNascita = nazioneNascita;
+        this.cittaNascita = cittaNascita;
+        this.dataNascita = dataNascita;
+    }
+
+    // getter e setter
+
+    public int getCodiceCliente() {
+        return codiceCliente;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNazioneNascita() {
+        return nazioneNascita;
+    }
+
+    public String getCittaNascita() {
+        return cittaNascita;
+    }
+
+    public String getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public void setCittaNascita(String cittaNascita) {
+        this.cittaNascita = cittaNascita;
+    }
+    public void setDataNascita(String dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+    public void setNazioneNascita(String nazioneNascita) {
+        this.nazioneNascita = nazioneNascita;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ areoporto/src/main/java/areoporto/Clienti.java
@@ -0,0 +1,69 @@
+package areoporto;
+
+import java.util.*;
+import java.io.*;
+// Classe Clienti
+class Clienti {
+    private List<Cliente> listaClienti;
+
+    public Clienti() {
+        listaClienti = new ArrayList<>();
+    }
+
+    public void aggiungiCliente(Cliente cliente) {
+        listaClienti.add(cliente);
+    }
+
+    public void eliminaCliente(int codiceCliente) {
+        listaClienti.removeIf(cliente -> cliente.getCodiceCliente()==codiceCliente);
+    }
+
+    public void modificaCliente(int codiceCliente, String nuovoCognome, String nuovoNome, String nuovaNazioneNascita, String nuovaCittaNascita, String nuovaDataNascita) {
+        for (Cliente cliente : listaClienti) {
+            if (cliente.getCodiceCliente() == codiceCliente) {
+                cliente.setCognome(nuovoCognome);
+                cliente.setNome(nuovoNome);
+                cliente.setNazioneNascita(nuovaNazioneNascita);
+                cliente.setCittaNascita(nuovaCittaNascita);
+                cliente.setDataNascita(nuovaDataNascita);
+                break;
+            }
+        }
+    }
+
+    public Cliente cercaClientePerCodice(int codiceCliente) {
+        for (Cliente cliente : listaClienti) {
+            if (cliente.getCodiceCliente() == codiceCliente) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    public List<Cliente> cercaClientePerCognomeENome(String cognome, String nome) {
+        List<Cliente> risultato = new ArrayList<>();
+        for (Cliente cliente : listaClienti) {
+            if (cliente.getCognome().equalsIgnoreCase(cognome) && cliente.getNome().equalsIgnoreCase(nome)) {
+                risultato.add(cliente);
+            }
+        }
+        return risultato;
+    }
+
+    public void salvaSuFile(String nomeFile) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomeFile))) {
+            oos.writeObject(listaClienti);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void ripristinaDaFile(String nomeFile) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomeFile))) {
+            listaClienti = (List<Cliente>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
